aa3f8e55c4c9b1682ebb9480a35dca4be30c6b81
Lazzwx <99898857+Lazzwx@users.noreply.github.com>
2022-09-28 10:31:59 +0300

    Update game.py

--- game.py
+++ game.py
@@ -2,6 +2,10 @@ import random
 turns = ['rock', 'paper', 'scissors']
 human_turns = []
 computer_turns = []
+draw_history = []
+win_history = []
+loss_history = []
+
 
 while(True):
     human_turn = input("Enter your turn, or type exit: ")
@@ -17,15 +21,21 @@ while(True):
     print(f'Human:{human_turn} vs. Computer:{computer_turn}')
     if human_turn == computer_turn:
         print('Draw!')
+        draw_history.append('Draw!')
     elif human_turn == 'rock' and computer_turn == 'scissors':
         print('Human wins!')
+        win_history.append('Win!')
     elif human_turn == 'paper' and computer_turn == 'rock':
         print('Human wins!')
+        win_history.append('Win!')
     elif human_turn == 'scissors' and computer_turn == 'paper':
         print('Human wins!')
+        win_history.append('Win!')
     else:
         print('Computer wins!')
+        loss_history.append('Loss!')
 
-print(f'You have played {len(human_turns)} times')
-print(human_turns)
-print(computer_turns)
+print(f'You have played {len(human_turns)} times!')
+print(f'You have won {len(win_history)} times!')
+print(f'You picked the same thing as the computer {len(draw_history)} times!')
+print(f'You have lost {len(loss_history)} times!')
